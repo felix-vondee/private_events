99fe6db811557080f728a3b3dd4f49ece366b54d
felix_vondee <felixvondee8@gmail.com>
2021-05-05 20:20:59 +0200

    Complete Users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-  has_many :events
+  has_many :events, foreign_key: :creator, class_name: 'Event'
+  has_many :attendees
+  has_many :attended_events, through: :attendees, source: :event
+
+  validates :name, presence: true, uniqueness: true
 end
