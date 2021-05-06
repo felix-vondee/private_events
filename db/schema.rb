1da9693ab5e932fa0b437028c08e6b0fe36cef3a
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-05-06 15:49:31 +0200

    Add date column to events table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_05_154249) do
+ActiveRecord::Schema.define(version: 2021_05_06_133813) do
 
   create_table "attendees", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2021_05_05_154249) do
     t.integer "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "date"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
