36910760e590b618b2a99aeba4887e62b05c0d53
bishoy-samwel <bishoy-samwuel@gmail.com>
2021-05-06 14:14:14 +0200

    add user_params

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+    def user_params #Extract the name of the user from the form
+        params.require(:user).permit(:name)
+    end
+
+    session[:current_user] = User.find(id)
+    session[:current_user]
 end
